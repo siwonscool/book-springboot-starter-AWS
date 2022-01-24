e36865290b472da94c115737f6373d00fc19c52a
siwonscool <kimsea13@naver.com>
2022-01-24 21:30:08 +0900

    게시글 등록화면 만들기

--- src/main/java/com/siwon/book/web/IndexController.java
+++ src/main/java/com/siwon/book/web/IndexController.java
@@ -10,4 +10,9 @@ public class IndexController {
     public String index(){
         return "index";
     }
+
+    @GetMapping("/post/save")
+    public String postsSave(){
+        return "posts-save";
+    }
 }
