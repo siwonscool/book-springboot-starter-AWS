d755b4c0330723978b5d7ee3e03cc3686431fde4
김시원 <kimsea13@naver.com>
2022-02-10 16:24:28 +0900

    구글 로그인, 로그아웃, 회원가입, 권한관리

--- src/main/java/com/siwon/book/web/IndexController.java
+++ src/main/java/com/siwon/book/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.siwon.book.web;
 
+import com.siwon.book.config.auth.dto.SessionUser;
 import com.siwon.book.service.posts.PostsService;
 import com.siwon.book.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     //머스테치 스타터 덕분에 컨트롤러에서 문자열을 반환할때 앞의 경로와 뒤의 파일 확장자는 자동으로 지정
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser sessionUser = (SessionUser) httpSession.getAttribute("user");
+        if (sessionUser != null){
+            model.addAttribute("userName",sessionUser.getName());
+        }
         return "index";
     }
 
